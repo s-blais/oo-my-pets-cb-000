ed253cdbce58737582cf2c4935f8f1ab01b31566
Stephen Blais <stephen@s-blais.com>
2019-09-02 19:27:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
   # code goes here
+  attr_accessor
+
+  def initialize
+    @species = "human"
+  end
+
+  def species #getter only because can't be changed
+    @species
+  end
+
+  def name #getter only because can't be changed
+    @name
+  end
+
+  def say_species
+    species
+  end
+    
+  
 end
