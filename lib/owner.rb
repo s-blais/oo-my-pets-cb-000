1c637e299eba39b882ed9f3178626d4131bf12dc
Stephen Blais <stephen@s-blais.com>
2019-09-02 20:20:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,8 +59,14 @@ class Owner
   end
 
   def sell_pets
-    self.cats.each {|cat| cat.mood = "nervous"}
-    self.dogs.each {|dog| dog.mood = "nervous"}
+    self.cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
   end
 
 
